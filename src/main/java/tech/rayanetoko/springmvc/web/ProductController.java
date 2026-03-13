7d39c42f596fe24cfe7389c5e4f8e1ca7d00fde2
ryntk <rayanetoko@proton.me>
2026-03-13 16:27:12 +0000

    add layout and new product form

--- src/main/java/tech/rayanetoko/springmvc/web/ProductController.java
+++ src/main/java/tech/rayanetoko/springmvc/web/ProductController.java
@@ -3,7 +3,12 @@ package tech.rayanetoko.springmvc.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
+
 import java.util.List;
 
 import tech.rayanetoko.springmvc.repositories.ProductRepository;
@@ -25,9 +30,31 @@ public class ProductController {
 		return "products";
 	}
 
+	@GetMapping("/")
+	public String home() {
+		return "redirect:/index";
+	}
+
 	@GetMapping("/delete")
 	public String delete(Long id) {
 		productRepository.deleteById(id);
 		return "redirect:/index";
+	}
+
+	@GetMapping("/new-product")
+	public String newProduct(Model model) {
+		model.addAttribute("product", new Product());
+		return "new-product";
+	}
+
+	@PostMapping("/save-product")
+	public String saveProduct(@Valid Product product, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "new-product";
+		}
+
+		productRepository.save(product);
+
+		return "redirect:index";
 	}
 }
